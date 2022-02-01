7a27408f22194670262e16cc06f18a907a895778
Md. Rokan Uzzaman <rokonuzzamancse1@gmail.com>
2022-02-01 13:21:35 +0600

    Login System integrated with API

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
         
         <input type="text" placeholder="Enter Email" v-model="email">
         <input type="password" placeholder="Enter Password" v-model="password">
-        <button class="loginbtn">Login</button>
+        <button class="loginbtn" v-on:click="login()">Login</button>
         <p>
             <router-link to="/signup">Sign Up Here</router-link>
         </p>
@@ -15,7 +15,34 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: "Login"
+    name: "Login",
+    data(){
+        return {
+            email:"",
+            password:""
+        }
+    },
+    methods:{
+       async login(){
+
+           let result = await axios.get(`http://localhost:3000/users?email=${this.email}&password=${this.password}`);
+           if(result.status==200 && result.data.length>0){
+               localStorage.setItem('user-info',JSON.stringify(result.data[0]));
+               this.$router.push({name:"Home"});
+           }
+        // console.log(result.data[0]);
+
+            //http://localhost:3000/users?email=ava@gmail.com&password=ava123
+        }
+    },
+
+    mounted(){
+        let userinfo = localStorage.getItem("user-info");
+        if(userinfo){
+            this.$router.push({name:"Home"});
+        }
+    }
 }
 </script>
