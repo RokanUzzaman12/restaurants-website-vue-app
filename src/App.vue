887b771bfb559a031cb4cf65a51b6a335c4ffeb6
Md. Rokan Uzzaman <rokonuzzamancse1@gmail.com>
2022-02-01 10:22:13 +0600

    Registation form added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <Signup/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Signup from './components/SignUp.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Signup
   }
 }
 </script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <img class="logo" src="../assets/res-logo.jpg">
+    <h1>Sigh Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name">
+        <input type="text" placeholder="Enter Email">
+        <input type="password" placeholder="Enter Password">
+        <button class="signup">Sign Up</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Signup"
+}
+</script>
+
+<style>
+    .logo{
+        width:100px;
+    }
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    margin-bottom: 30px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+    border-radius: 20px;
+}
+
+.signup {
+    width: 325px;
+    height: 40px;
+    background-color: skyblue;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+}
+</style>
