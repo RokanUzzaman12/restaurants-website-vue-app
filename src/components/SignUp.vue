cb4424a2cdda329f89a577a9e4432c88fabdf684
Md. Rokan Uzzaman <rokonuzzamancse1@gmail.com>
2022-02-04 23:49:31 +0600

    Check email exists or not on database

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,6 +2,7 @@
 <div>
     <img class="logo" src="../assets/res-logo.jpg">
     <h1>Sigh Up</h1>
+    <p class="errorcolor" v-for="item in error" :key="item">{{item}} is required</p>
     <div class="register">
         <input type="text" placeholder="Enter Name" v-model="name">
         <input type="text" placeholder="Enter Email" v-model="email">
@@ -15,51 +16,71 @@
 </template>
 
 <script>
-
 import axios from 'axios'
 export default {
 
     name: "Signup",
-    data(){
-        return{
-            name:"",
-            email:"",
-            password:"",
-            error:[]
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            error: []
         }
     },
-    methods:{
-        async getUserData(){
-            
+    methods: {
+        async getUserData() {
+            this.error = [];
+            if (!this.name == '' && !this.email == '' && !this.password == 0) {
+                let checkData = await axios.get(`http://localhost:3000/users?email=${this.email}`)
+                console.log(checkData.data.length);
 
-            let result = await axios.post("http://localhost:3000/users",{
-                name:this.name,
-                email:this.email,
-                password:this.password
-            });
-            //console.log("submit Result",result);
-            // console.log("submit Result",result.status);
+                if (checkData.data.length == 0) {
+                    let result = await axios.post("http://localhost:3000/users", {
+                        name: this.name,
+                        email: this.email,
+                        password: this.password
+                    });
+                    //console.log("submit Result",result);
+                    // console.log("submit Result",result.status);
 
-            if(result.status==201){
-                alert("Signup done");
-                this.$router.push({name:"Home"});
+                    if (result.status == 201) {
+                        alert("Signup done");
+                        this.$router.push({ name: "Home" });
+                    }
+                    localStorage.setItem("user-info", JSON.stringify(result.data));
+                } else {
+                    alert("Email exist");
+                    this.$router.push({ name: "Login" });
+                }
             }
-            localStorage.setItem("user-info",JSON.stringify(result.data));
+            if (this.name == '') {
+                this.error.push('Name');
+            }
 
-            
+            if (this.email == '') {
+                this.error.push('Email');
+            }
+
+            if (this.password == '') {
+                this.error.push('Password');
+            }
+
         }
     },
 
-    mounted(){
+    mounted() {
         let userdata = localStorage.getItem("user-info");
 
-        if(userdata){
-            this.$router.push({name:"Home"});
+        if (userdata) {
+            this.$router.push({ name: "Home" });
         }
     }
 }
 </script>
 
 <style>
-
+.errorcolor {
+    color: red;
+}
 </style>
